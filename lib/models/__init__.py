38bfde6403c801efa1f03f01633df0cbba796385
Emily Valdez <emily.milan.valdez@gmail.com>
2023-11-06 14:01:34 -0600

    renamed files and linked holidays.db

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,9 +1,10 @@
 import sqlite3
 
 # this connects us to the database
-CONN = sqlite3.connect('company.db')
+CONN = sqlite3.connect('holidays.db')
 # this creates a tool where we can talk to the database
 CURSOR = CONN.cursor()
+
 
 
 
